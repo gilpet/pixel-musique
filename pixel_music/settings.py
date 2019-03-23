e775853c380d1ed66b88a9d557a0be6d4bdd96b8
Peter Gillis <petergillis11@gmail.com>
2019-03-24 04:46:04 +0700

    Update settings.py

--- pixel_music/settings.py
+++ pixel_music/settings.py
@@ -1,16 +1,9 @@
 import os
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '6+&!u=$ghdh2^p7tj97wc7+rur2z9absx%%%h5v2hv-nnauy2f'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
